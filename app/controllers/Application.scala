4289d22542d5cd4faa5758825db432aa7892e07f
Christopher Cundill <christophercundill@gmail.com>
2012-11-20 21:55:40 +0000

    Integrated Twitter bootstrap UI for easy styling along with some preliminary text for the home, about and contact pages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,19 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
+  }
+
+  def about = Action {
+    Ok(views.html.about())
+  }
+
+  def contact = Action {
+    Ok(views.html.contact())
   }
   
 }
